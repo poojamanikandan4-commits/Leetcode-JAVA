f6b530f99abdad4b1702105f5954ebab6d4465e2
Pooja Manikandan <poojavsb074@gmail.com>
2026-08-05 11:41:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (76.79%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.java
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -1,19 +1,12 @@
-class Solution {
+public class Solution {
     public String convertToTitle(int columnNumber) {
         StringBuilder result = new StringBuilder();
-        
         while (columnNumber > 0) {
             columnNumber--;
-            
-            int remainder = columnNumber % 26;
-            
-            char letter = (char) ('A' + remainder);
-            
-            result.insert(0, letter);
-            
+            char c = (char) ('A' + columnNumber % 26);
+            result.insert(0, c);
             columnNumber /= 26;
         }
-        
         return result.toString();
     }
 }
